2c08e10cf4798c483bf00f9b1c8b250e85e0bf63
HannesLeber <Hannes.Leber@hotmail.com>
2023-03-10 20:52:44 +0100

    Server-communication implemented on click, Lable changed

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText matrikelnummer = (EditText) findViewById(R.id.matrikelnummer);
+        Button abschicken = (Button) findViewById(R.id.abschicken);
+        TextView servertext = (TextView) findViewById(R.id.servertext);
+
+        abschicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String matrikelnr = matrikelnummer.getText().toString();
+                //has to be final, so that the Thread can access it
+                final String[] s = new String[1];
+                Thread t = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try {
+                            Socket socket = new Socket("se2-isys.aau.at", 53212);
+                            DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+                            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            Log.d("", "Sende an Server: " + matrikelnr);
+                            outToServer.writeBytes(matrikelnr+"\n");
+                            String serverAntwort = inFromServer.readLine();
+                            Log.d("", "Serverantwort: "+ serverAntwort);
+                            socket.close();
+
+                            s[0] = serverAntwort;
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                t.start();
+
+
+                try {
+                    t.join(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                servertext.setText(s[0]);
+            }
+        });
+
     }
+
+    public String getServerAnswer(String matrikelnummer) {
+
+
+
+        return "";
+
+
+    }
+
+
+
+
+
+
+
 }
